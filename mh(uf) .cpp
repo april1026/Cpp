8f0a20d6750d02c819149a9ee0902a320ad8c904
april1026 <36957874+april1026@users.noreply.github.com>
2018-04-27 10:00:25 +0800

    Update mh(uf) .cpp

--- mh(uf) .cpp
+++ mh(uf) .cpp
@@ -9,8 +9,7 @@ int main() {
 		listnum *= 2;
 	}
 	char like[m][2][2];
-	char mh[n][listnum];
-	
+	int mh[n][listnum];
 	
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < 2; j++) {
@@ -23,8 +22,28 @@ int main() {
 		}
 	}
 	
+	int change = 1;
+	bool save = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < listnum; j++) {
+			if (i == 0)
+				mh[i][j] = j % 2; 
+			else if (j % change == 0){
+				if (save == 0)
+					save = 1;
+				else if (save == 1)
+					save = 0;
+				mh[i][j] = save;
+			}else
+				mh[i][j] = save;
+		}
+		change *= 2;
+		save = 1;
+	}
 	
 	
+	
+	return 0;
 }
 //https://zerojudge.tw/ShowProblem?problemid=b065
 //https://tw.answers.yahoo.com/question/index?qid=20090419000015KK08872
