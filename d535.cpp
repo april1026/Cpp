07b7eb01bf652ace739817ec5c4c6b8835c82f65
april1026 <36957874+april1026@users.noreply.github.com>
2018-05-10 21:13:48 +0800

    Create d535.cpp

---
+++ d535.cpp
@@ -0,0 +1,37 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+	char number[100];
+	int total_number;
+	cin >> number;
+	for (int i = 0; i < 100; i++) {
+		if (number[i] == '\0') {
+			total_number = i;
+			break;
+		}
+	}
+	int password = 0;
+	
+	int num[total_number];
+	for (int i = 0; i < total_number; i++) {
+		num[i] = int(number[i]) - '0';
+		cout << num[i];
+	}
+	
+	for (int i = 0; i < total_number; i++) {
+		if ( (num[i] * 2 >= num[i+1]) && (num[i] == num[total_number - i - 1]) ) {
+			if (num[i] % 2 == 0)
+				password = (password * 10 + num[i]);
+		}else {
+			cout << "INCORRECT" << endl;
+			password = -1;
+			break;
+		} 	
+	}
+	
+	if (password > 0)
+		cout << password;
+	
+ return 0; 
+}
