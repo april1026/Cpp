60f120c5251da60feb8d74423faf88e8f90577f8
april1026 <36957874+april1026@users.noreply.github.com>
2018-05-18 10:00:27 +0800

    Create c461(90%).cpp

---
+++ c461(90%).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int result;
+	cin >> result;
+	
+	int output = 0;
+	if ((a & b) == result){
+		cout << "AND\n";
+		output++;
+	}
+	if ((a | b) == result){
+		cout << "OR\n";
+		output++;
+	}
+	if ((a ^ b) == result){
+		cout << "XOR\n";
+		output++;
+	}
+	if (output == 0)
+	 cout << "IMPOSSIBLE";
+}
