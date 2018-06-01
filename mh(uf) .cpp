40ab02f4a0fa79690367db662b7a001b6ce9e7fc
april1026 <36957874+april1026@users.noreply.github.com>
2018-06-01 09:58:39 +0800

    Update mh(uf) .cpp

--- mh(uf) .cpp
+++ mh(uf) .cpp
@@ -4,17 +4,18 @@ int main() {
 	
 	int k, n, m;
 	cin >> k >> n >> m;
-	int listnum = 1;
+	
+	int listnum = 1; //幾種可能性 
 	for (int i = 0; i < n; i++) {
 		listnum *= 2;
-	}
-	char like[m][2][2];
-	int mh[n][listnum];
+	} //2的n次方種可能性 
 	
+	int like[m][2][2];
 	for (int i = 0; i < m; i++) {
+		char input[3];
 		for (int j = 0; j < 2; j++) {
-			cin >> like[i][j];
-			if (like[i][j][0] == 'm') {
+			cin >> input;
+			if (input[0] == 'm') {
 				like[i][j][0] = 0;
 			}else {
 				like[i][j][0] = 1;
@@ -22,6 +23,7 @@ int main() {
 		}
 	}
 	
+	int mh[n][listnum];
 	int change = 1;
 	bool save = 0;
 	for (int i = 0; i < n; i++) {
@@ -41,7 +43,20 @@ int main() {
 		save = 1;
 	}
 	
-	
+	bool result[listnum] = 0;
+	for (int i = 0; i < listnum; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < m; k++) {
+				if (mh[ like[k][0][1] ][i] == like[k][0][0]) {
+					result[i] = 1;
+					break;
+				}else if (mh[ like[k][1][1] ][i] == like[k][1][0]) {
+					result[i] = 1;
+					break;
+				}
+			}
+		}
+	}
 	
 	return 0;
 }
