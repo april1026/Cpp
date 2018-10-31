dd7e507166b658bfb5581424116176610349963d
april1026 <36957874+april1026@users.noreply.github.com>
2018-10-31 15:22:41 +0800

    Create 106 - 3(uf).cpp

---
+++ 106 - 3(uf).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int input[n];
+    int key[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> input[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> key[i];
+    }
+    
+    int yet[n];
+    for (int i = 0; i < n; i++) {
+        yet[i] = key[i] - input[i];
+        if ()
+    }
+
+    cout << output;
+
+    return 0;
+}
