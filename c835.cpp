d7dec533109cb7b47fe042cffc325d8dd8eaac96
april1026 <36957874+april1026@users.noreply.github.com>
2019-05-26 15:02:46 +0800

    Update c835.cpp

--- c835.cpp
+++ c835.cpp
@@ -10,24 +10,24 @@ int main() {
 	for (int i = 0; i < M; i++)
 		w *= 2;
 	
-	long long int dp[w] = {};
+	long int dp[w+1] = {};
 	for (int i = 0; i < N; i++) {
-		long long int a, b;
+		long int a, b;
 		cin >> a >> b;
 		int aa = 1;
 		for (int j = 0; j < a; j++)
 			aa *= 2;
-		for (int j = (w-1); j >= aa; j--) {
-			dp[j] = max(dp[j-aa], dp[j-aa]+b);
+		for (int j = w; j >= aa; j--) {
+			dp[j] = max(dp[j], dp[j-aa]+b);
 		}
-		for (int j = 0; j < w; j++)
-			cout << dp[j] << " ";
-		cout << "\n";
+//		for (int j = 1; j <= w; j++)
+//			cout << dp[j] << " ";
+//		cout << "\n";
 	}
 	
-	long long int ans = 0;
-	for (int i = 0; i < w; i++) {
-		cout << dp[i] << " ";
+	long int ans = 0;
+	for (int i = 1; i <= w; i++) {
+//		cout << dp[i] << " ";
 		ans = max(ans, dp[i]);
 	}
 	
